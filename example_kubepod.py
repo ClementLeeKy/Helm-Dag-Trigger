41ab6afb146f6ea556ef272b3156d9073aee2b3d
Clement Lee <64537371+ClementLeeKy@users.noreply.github.com>
2021-05-05 11:17:15 +0800

    Update example_kubepod.py

--- example_kubepod.py
+++ example_kubepod.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
